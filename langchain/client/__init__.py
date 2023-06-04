7eadf04a23827aac11544a7ff62c338d1b329690
vowelparrot <130414180+vowelparrot@users.noreply.github.com>
2023-06-04 13:49:25 -0700

    Update imports

--- langchain/client/__init__.py
+++ langchain/client/__init__.py
@@ -1,6 +1,11 @@
 """LangChain+ Client."""
 
 
-from langchain.client.langchain import LangChainPlusClient
+from langchain.client.runner_utils import (
+    arun_on_dataset,
+    run_on_dataset,
+    arun_on_examples,
+    run_on_examples,
+)
 
-__all__ = ["LangChainPlusClient"]
+__all__ = ["arun_on_dataset", "run_on_dataset", "arun_on_examples", "run_on_examples"]
